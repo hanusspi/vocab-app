6005fefcb02d7c475b0a535de93a1c3f4feb30b1
hanusspi <95216134+hanusspi@users.noreply.github.com>
2025-08-09 06:09:33 +0200

    first commit

---
+++ sw.js
@@ -0,0 +1,51 @@
+const CACHE_NAME = 'vocab-app-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-128.png',
+  './icon-512.png'
+];
+
+// Install Service Worker
+self.addEventListener('install', function(event) {
+  console.log('Service Worker installing...');
+  
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch from cache when offline
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Return cached version or fetch from network
+        return response || fetch(event.request);
+      }
+    )
+  );
+});
+
+// Update Service Worker
+self.addEventListener('activate', function(event) {
+  console.log('Service Worker activating...');
+  
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Deleting old cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
